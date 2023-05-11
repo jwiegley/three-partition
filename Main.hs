f898d93d98f1c53b9fb0d22e8e9f275976c42bf3
John Wiegley <johnw@newartisans.com>
2023-05-10 22:45:00 -0700

    Finished first version

--- Main.hs
+++ Main.hs
@@ -1,280 +1,132 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
--- import Data.Csv
-
 import Control.Exception (assert)
 import Data.Foldable
-import Data.List (genericLength, intercalate, nub, tails)
-import Data.Map (Map)
-import Data.Map qualified as Map
+import Data.List (genericLength, sortOn)
 import Data.Proxy
 import Data.Reflection
 import Data.SBV
-import Data.Time
 import Prelude hiding (pi)
 
+-- | A solution is a sequence of set assignments for each input
+data Solution s v = Solution
+  { assignments :: [v],
+    sums :: [v]
+  }
+  deriving (Show)
+
+-- We inject the size of the input list, in order to know how to group the
+-- variables coming from the solver.
+instance (SatModel v, Reifies s Int) => SatModel (Solution s v) where
+  parseCVs as = do
+    let len = reflect (Proxy :: Proxy s)
+        n = len `div` 3
+    (a, bs) <- parseCVs as
+    return
+      ( Solution
+          (take len a)
+          (take n (drop len a)),
+        bs
+      )
+
 isValid ::
-  [Int] -> -- input values
-  [SWord8] -> -- mapping
+  [Int] ->
+  Solution s SWord32 ->
   SBool
-isValid maxGroupSize g p s =
+isValid i Solution {..} =
   assert
-    ( length (solAssignments s) == length p
-        && length (solBlackFacilitators s) == length g
-        && length (solFacilitators s) == length g
-        && length (solParticipants s) == length g
+    ( length assignments == length i
+        && length sums == n
+        && length i `rem` 3 == 0
     )
-    $
-    -- Every participant is assigned to an applicable group, and the
-    -- constraints hold for being associated with that group
-    ala
-      sAnd
-      solAssignments
-      ( \pi x ->
-          x .>= 0
-            .&& x .< genericLength g
-            .&& sAll
-              ( \gi ->
-                  fromIntegral gi .== x
-                    .=> eachParticipant (p !! pi) x (g !! gi) gi
-              )
-              [0 .. length g - 1]
-      )
-      -- Track how many facilitators are in each group
-      .&& ala sAnd solFacilitators (\gi x -> x .== facilitators gi)
-      -- Track how many black facilitators are in each group
-      .&& ala sAnd solBlackFacilitators (\gi x -> x .== blackFacilitators gi)
-      -- Track how many participants are in each group
-      .&& ala sAnd solParticipants (\gi x -> x .== participants gi)
-      -- Track how many black participants are in each group
-      .&& ala sAnd solBlackParticipants (\gi x -> x .== blackParticipants gi)
-      -- Ensure correct group sizes
-      .&& sAll
-        ( \gi ->
-            (participants gi .== 0 .&& facilitators gi .== 0)
-              .|| ( participants gi .> 0
-                      .&& participants gi .<= fromIntegral maxGroupSize
-                      .&& facilitators gi .>= 2
-                      .&& blackFacilitators gi .>= 1
-                  )
-        )
-        [0 .. length g - 1]
-      -- All pairings are honored
-      .&& sAll
-        ( \(i, j) ->
-            solAssignments s !! i .== solAssignments s !! j
-        )
-        (pairings pPairWith p)
-      .&& sAll
-        ( \(i, j) ->
-            solAssignments s !! i ./= solAssignments s !! j
-        )
-        (pairings pDoNotPairWith p)
-  where
-    eachParticipant Participant {..} x Group {..} gi =
-      fromBool
-        ( gDayOfWeek
-            `elem` map
-              availDayOfWeek
-              pAvailability
-            && any
-              (canMeet pIsFacilitator gStartTime)
-              pAvailability
-        )
-        .&& maybe
-          minBound
-          fromIntegral
-          pPrefereredMinGroupSize
-          .<= solParticipants s !! gi
-        .&& maybe
-          maxBound
-          fromIntegral
-          pPrefereredMaxGroupSize
-          .>= solParticipants s !! gi
-        .&& x .== maybe x fromIntegral pFixed
-
-    ala k acc f = k (zipWith f [0 ..] (acc s))
-
-    participants = countParticipants (const True)
-    blackParticipants = countParticipants pIsBlack
-    facilitators = countParticipants pIsFacilitator
-    blackFacilitators =
-      countParticipants (\i -> pIsFacilitator i && pIsBlack i)
-
-    countParticipants :: (Participant -> Bool) -> Int -> SWord8
-    countParticipants f gi =
-      ala
-        sum
-        solAssignments
-        ( \pi a ->
-            oneIf
-              ( fromIntegral gi .== a
-                  .&& fromBool (f (p !! pi))
-              )
-        )
-
-showSchedule :: [Participant] -> [Group] -> Solution s Word8 -> String
-showSchedule p g s =
-  unlines $ intercalate [""] $ map f assigned
-  where
-    assigned = nub (solAssignments s)
-
-    f :: Word8 -> [String]
-    f gi =
-      [ gName (g !! fromIntegral gi),
-        "===================="
-      ]
-        ++ concat
-          ( zipWith
-              (\pi i -> [getName (p !! pi) | i == gi])
-              [0 ..]
-              (solAssignments s)
+    $ sAnd
+      [ sAll (\x -> x .>= 0 .&& x .< n) assignments,
+        sAll
+          ( \ci ->
+              sum (map (\a -> oneIf (ci .== a)) assignments) .== (3 :: SWord32)
           )
-
-    getName i =
-      ( if pIsBlack i
-          then "*"
-          else " "
-      )
-        ++ ( if pIsFacilitator i
-               then "F"
-               else " "
-           )
-        ++ " "
-        ++ pName i
-
-scheduleGroups :: Size -> [Participant] -> IO ()
-scheduleGroups maxGroupSize p = do
-  let g = determineGroups p
-  putStrLn "Finding scheduling solution...\n"
-  reify (length g, length p) $ \(Proxy :: Proxy s) -> do
-    LexicographicResult res <- optimize Lexicographic $ do
-      solAssignments <- mkFreeVars (length p)
-      solBlackFacilitators <- mkFreeVars (length g)
-      solFacilitators <- mkFreeVars (length g)
-      solBlackParticipants <- mkFreeVars (length g)
-      solParticipants <- mkFreeVars (length g)
-      constrain $ isValid maxGroupSize g p Solution {..}
-      minimize "number-facilitators" $ foldl' smax 0 solFacilitators
-      maximize "balance-participants" $ foldl' smin 0 solBlackParticipants
-    case extractModel res :: Maybe (Solution s Word8) of
-      Nothing -> error "No model found"
-      Just model -> dispSolution g model
+          [0 .. pred (genericLength sums)],
+        ala
+          sAnd
+          ( \ci x ->
+              x
+                .== ala
+                  sum
+                  (\ai a -> ite (ci .== a) (fromIntegral (i !! ai)) 0)
+                  assignments
+          )
+          sums
+      ]
   where
-    dispSolution :: [Group] -> Solution s Word8 -> IO ()
-    dispSolution g model = do
-      putStr $ showSchedule p g model
+    n :: Num a => a
+    n = fromIntegral (length i `div` 3)
+
+    ala k f = k . zipWith f [0 ..]
+
+intoSets :: [Int] -> Solution s Word32 -> [[Int]]
+intoSets i Solution {..} = map f assigned
+  where
+    assigned =
+      reverse (map fst (sortOn snd (zip ([0 ..] :: [Word32]) sums)))
+    f :: Word32 -> [Int]
+    f gi =
+      concat $
+        zipWith
+          (\ai x -> [i !! ai | x == gi])
+          [0 ..]
+          assignments
+
+partition :: [Int] -> IO ()
+partition i = do
+  putStrLn "Finding partition solution...\n"
+  reify (length i) $ \(Proxy :: Proxy s) -> do
+    LexicographicResult res <- optimize Lexicographic $ do
+      let n = length i `div` 3
+      assignments <- mkFreeVars (length i)
+      sums <- mkFreeVars n
+      constrain $ isValid i Solution {..}
+      minimize "set-sums" $ foldl' smax 0 sums
+    maybe
+      (error "No model found")
+      dispSolution
+      (extractModel res :: Maybe (Solution s Word32))
+  where
+    dispSolution model = do
+      print $ intoSets i model
+      print $ map sum (intoSets i model)
       putStrLn $
         "\nValid: "
-          ++ show (isValid maxGroupSize g p (literalize model))
+          ++ show (isValid i (literalize model))
+      print model
       where
         literalize s =
           s
-            { solAssignments = map literal (solAssignments s),
-              solBlackFacilitators = map literal (solBlackFacilitators s),
-              solFacilitators = map literal (solFacilitators s),
-              solBlackParticipants = map literal (solBlackParticipants s),
-              solParticipants = map literal (solParticipants s)
+            { assignments = map literal (assignments s),
+              sums = map literal (sums s)
             }
-
--- Given a CSV file of the proper schedule, generate and display an updated
--- version of that CSV file which assigns participants to groups.
-c2gSchedule :: FilePath -> IO ()
-c2gSchedule _ = undefined
 
 main :: IO ()
 main = do
-  -- print $ pairings pPairWith participants
-  -- print $ pairings pDoNotPairWith participants
-  scheduleGroups 20 participants
-  where
-    participants =
-      [ Participant
-          { pName = "Aaron",
-            pIsFacilitator = True,
-            pIsBlack = False,
-            pAvailability =
-              [ Available Thursday 1200 2000,
-                Available Friday 1200 2000,
-                Available Saturday 1200 2000
-              ],
-            pPairWith = ["Susan"],
-            pDoNotPairWith = [],
-            pPrefereredMinGroupSize = Nothing,
-            pPrefereredMaxGroupSize = Nothing,
-            pFixed = Nothing
-          },
-        Participant
-          { pName = "Regina",
-            pIsFacilitator = True,
-            pIsBlack = True,
-            pAvailability =
-              [ Available Thursday 1200 2000,
-                Available Friday 1200 2000,
-                Available Saturday 1200 2000
-              ],
-            pPairWith = [],
-            pDoNotPairWith = [],
-            pPrefereredMinGroupSize = Nothing,
-            pPrefereredMaxGroupSize = Nothing,
-            pFixed = Nothing
-          },
-        Participant
-          { pName = "John",
-            pIsFacilitator = True,
-            pIsBlack = False,
-            pAvailability =
-              [ Available Monday 1200 2000,
-                Available Tuesday 1200 2000,
-                Available Wednesday 1200 2000,
-                Available Thursday 1200 2000
-              ],
-            pPairWith = [],
-            pDoNotPairWith = [],
-            pPrefereredMinGroupSize = Nothing,
-            pPrefereredMaxGroupSize = Nothing,
-            pFixed = Nothing
-          },
-        Participant
-          { pName = "Cherlynn",
-            pIsFacilitator = True,
-            pIsBlack = True,
-            pAvailability =
-              [ Available Monday 1200 2000,
-                Available Tuesday 1200 2000,
-                Available Wednesday 1200 2000
-              ],
-            pPairWith = [],
-            pDoNotPairWith = [],
-            pPrefereredMinGroupSize = Nothing,
-            pPrefereredMaxGroupSize = Nothing,
-            pFixed = Nothing
-          },
-        Participant
-          { pName = "Susan",
-            pIsFacilitator = True,
-            pIsBlack = False,
-            pAvailability =
-              [ Available Monday 1200 2000,
-                Available Tuesday 1200 2000,
-                Available Wednesday 1200 2000,
-                Available Thursday 1200 2000,
-                Available Friday 1200 2000,
-                Available Saturday 1200 2000
-              ],
-            pPairWith = [],
-            pDoNotPairWith = [],
-            pPrefereredMinGroupSize = Nothing,
-            pPrefereredMaxGroupSize = Nothing,
-            pFixed = Nothing
-          }
-      ]
+  let l =
+        [ 10,
+          100,
+          82,
+          39,
+          87,
+          12,
+          88,
+          63,
+          95,
+          61,
+          88
+        ]
+      l' = l ++ replicate ((3 - length l `rem` 3) `mod` 3) 0
+  print l'
+  partition l'
